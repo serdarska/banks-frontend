8a61e7147427a70c52c77369ae5215663b69e641
serdarska <serdarska.jovana.js@gmail.com>
2022-12-18 19:22:15 +0100

    made the team component responsive

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,21 +1,21 @@
 <template>
   <section class="bg-light">
-    <div class="row mt-5 pt-5 mx-0 px-0">
+    <div class="row  mx-0 px-0">
       <div class="col-8 p-0">
-        <div class="position-absolute  d-none d-xl-block">
+        <div class="position-absolute  d-none d-xl-block d-none d-lg-block">
           <img src="../assets/images/gray-triangle.svg" alt="Grey Triangle" height="700">
         </div>
-        <div class="text-center text-black-50 text-size mt-5 pt-5 me-5 pe-5 position-relative">
-        Find your bank <br/>
-        on the go!
-      </div>
+        <div class="text-center text-black-50 text-size text-uppercase me-5 pe-5 top-40 position-relative">
+          Find your bank <br/>
+          on the go!
+        </div>
       </div>
       <div class="col-4 align-items-end p-0">
-        <div class="position-absolute start-60">
+        <div class="position-absolute start-48  d-none d-lg-block">
           <img src="../assets/images/green-triangle.svg" alt="Green Triangle" height="700">
         </div>
-        <div class=" ratio ratio-695x742">
-        <img src="../assets/images/bank.svg" alt="Bank" class="align-items-end">
+        <div class="ratio ratio-695x742  d-none d-lg-block ">
+          <img src="../assets/images/bank.svg" alt="Bank" class="align-items-end top-15">
         </div>
       </div>
     </div>
@@ -29,10 +29,19 @@ export default {
 </script>
 
 <style scoped>
-.text-size{
+.text-size {
   font-size: 50px;
 }
-.start-60{
+
+.start-48 {
   left: 48%;
+}
+
+.top-40 {
+  top: 40%;
+}
+
+.top-15 {
+  top: 15%;
 }
 </style>
--- components/TeamComponent.vue
+++ components/TeamComponent.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="bg-light">
     <div class="row">
-      <div class="col-4 ms-5 ps-5 mt-5 pt-5">
-        <p class="background text-center rounded-4 fs-3 text-black-50 me-4 px-2 py-3">
+      <div class="col-12 col-sm-8 justify-content-center ps-sm-5 ms-sm-5 mt-sm-5 me-sm-3 col-lg-4 ms-lg-5 ps-lg-5 mt-lg-5 pt-lg-5">
+        <p class="background text-center rounded-4 fs-3 text-black-50 me-4 px-2 py-3 start-10">
           Our goal is to help you find<br/>
           the bank you have been<br/>
           looking for in an easier and<br/>
@@ -19,8 +19,9 @@
           location. <br/>
         </p>
       </div>
-      <div class="col-8 p-0">
-        <img src="../assets/images/team.svg" alt="Team" class="position-absolute bottom-0 end-0 " height="650" width="330">
+      <div class="col-8 p-0 d-none d-lg-block">
+        <img src="../assets/images/team.svg" alt="Team" class="position-absolute  bottom-0 end-0 top-20" height="630"
+             width="330">
       </div>
     </div>
   </section>
@@ -33,7 +34,12 @@ export default {
 </script>
 
 <style scoped>
-.background-color-green{
-  background:#3cb371;
+.background-color-green {
+  background: #3cb371;
 }
+
+.top-20 {
+  top: 20%;
+}
+
 </style>
