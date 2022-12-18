3b08c64173fe0d00ca4059e5365c2635323513ad
serdarska <serdarska.jovana.js@gmail.com>
2022-12-18 10:41:18 +0100

    backend changes

--- pages/banks.vue
+++ pages/banks.vue
@@ -1,23 +1,43 @@
 <template>
-<div>
-  <custom-navbar class="z-index-0" />
-  <banks-component class="z-index-1"/>
-</div>
+  <div>
+    <custom-navbar/>
+    <banks-component :banks="banks" @set-filters="load($event)"/>
+  </div>
 </template>
 
 <script>
 export default {
-name: "banks"
+  name: "banks",
+  data() {
+    return {
+      banks: [],
+      filters: {
+        s: ''
+      }
+    }
+  },
+
+  async mounted() {
+    await this.load(this.filters);
+  },
+  methods: {
+    async load(f) {
+      this.filters.s = f.s;
+      const arr = [];
+
+      if (this.filters.s) {
+        arr.push('s=${this.filters.s}');
+      }
+
+      const response = await fetch('http://localhost:8080/api/banks/');
+      const content = await response.json();
+      this.banks = content.data;
+
+    }
+  }
 }
 </script>
 
 <style scoped>
 
-.z-index-0 {
-  z-index: 0;
-}
-
-.z-index-1 {
-  z-index: 1;
-}
 </style>
